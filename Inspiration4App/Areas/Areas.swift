2608dcd01332a8df2764c7002a74feeaa6d7ad39
SangUk Park <parksang1993@gmail.com>
2023-11-16 22:55:32 +0900

    Add Areas Group

---
+++ Inspiration4App/Areas/Areas.swift
@@ -0,0 +1,18 @@
+//
+//  Areas.swift
+//  Inspiration4App
+//
+//  Created by 박상욱 on 11/16/23.
+//
+
+import SwiftUI
+
+struct Areas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Areas()
+}
